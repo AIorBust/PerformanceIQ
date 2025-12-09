e831b31e181d05bb50fa6e57d2f64af413c96eba
LoganFleck44 <Logandfleck@gmail.com>
2025-12-08 21:54:31 -0500

    Update function_app.py
    commit

--- function_app.py
+++ function_app.py
@@ -7,9 +7,6 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def PerformanceIQ_ReadJson(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-
--- jf commnent
-
     name = req.params.get('name')
     if not name:
         try:
@@ -23,6 +20,6 @@ def PerformanceIQ_ReadJson(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Provide a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
